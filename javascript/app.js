bfa033db5e70d5c5ab0b53c317737a4eebd899fc
hafed <hafedrs@gmail.com>
2022-12-23 01:58:14 +0200

    fix the design for multi post

--- javascript/app.js
+++ javascript/app.js
@@ -30,7 +30,7 @@ const worksArray = [
     title: "Tonic",
     experiences: ["Canopy", "Back End Dev", "2015"],
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     technologies: ["html", "css", "javascript", "github", "ruby", "Bootstraps"],
     urlMobile: "/images/work_1.jpg",
     urlDesktop: "/images/work_1_lg.png",
